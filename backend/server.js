b3496f9ebe676be273779b6e46c2a07081b2d207
akashhelawar21@gmail.com <akashhelawar21@gmail.com>
2024-11-15 02:23:42 +0530

    changes for get product

--- backend/server.js
+++ backend/server.js
@@ -12,12 +12,12 @@ const __dirname = path.resolve();  // This gets the current directory
 const PORT = process.env.PORT || 5000;
 const app = express();
 
-if(process.env.NODE_ENV ==="production"){
-    app.use(express.static(path.join(__dirname, "/frontend/build")))
-    app.get("*",(req,res)=>{
-      res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))  
-    })
-}
+// if(process.env.NODE_ENV ==="production"){
+//     app.use(express.static(path.join(__dirname, "/frontend/build")))
+//     app.get("*",(req,res)=>{
+//       res.sendFile(path.resolve(__dirname, "frontend","build","index.html"))  
+//     })
+// }
 
 // Enable CORS with the specific frontend URL
 app.use(cors({
